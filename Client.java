40c3c445dfba44973ec0d8183a87bb77fd3a5481
Inpanic4 <sargyropoulos@citycollege.sheffield.eu>
2020-12-12 19:52:48 +0200

    1st commit

---
+++ Client.java
@@ -0,0 +1,165 @@
+/*
+ * Filename: UDPEchoClient.java
+ * Description: An echo client using connectionless delivery system (UDP).
+ *              Sends character messages to a server which are echoed capitalized.
+ *              No error handling and exceptions are implemented.
+ * Operation: java UDPEchoCLient [hostname] [port]
+ *
+ * Author: Thanos Hatziapostolou
+ * Module: Network Computing
+ */
+
+/* for the use of arraylist and scanner */
+import java.util.*;
+
+import java.net.*;
+import java.io.*;
+
+public class Client {
+private static ArrayList<Integer> AuctionNumbers = new ArrayList<Integer>();
+private String itemName;
+private String itemDescription;
+private int itemStartPrice;
+private boolean typeOfClosingTheAuction;
+	
+/*Argyropoulos! This is a method in order to generate an AuctionID when a client is listing an item *
+/It is not possible to generate the same number!!!! but there is a limited range of numbers from 1000 to 10000*/
+	
+	public static void generateAuctionID() {
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		Random random = new Random();
+			for (int i=1000; i<=10000; i++)
+    		numbers.add(i);
+			Collections.shuffle(numbers);
+			// System.out.println(numbers.get(0));
+			AuctionNumbers.add(numbers.get(0));
+			System.out.println(numbers.get(0));
+
+	}
+
+	/*Argyropoulos! a method where the user will be able to list an item for an auction */
+	public  static void listItem() {
+		Scanner scanner = new Scanner(System.in);
+		/*Argyropoulos! Have to make exceptions if a user enters int instead of string and so on */
+		System.out.println("Enter the name of the Item ");
+		String itemName = scanner.nextLine();
+
+		System.out.println("Enter a description for your item");
+		String itemDescription = scanner.nextLine();
+
+		System.out.println("Enter your item's Starting price");
+		int itemStartPrice = scanner.nextInt();
+
+		//Argyropoulos! (for later) System.out.println("Enter the type of the auction");
+
+		System.out.println("Thank you theese are the cresendials you entered Item name:" + itemName +(" Item Description:")  +itemDescription+(" itemStartPrice:") +itemStartPrice);
+
+		System.out.println("This is the auctionID");
+		generateAuctionID();
+	}
+
+
+	/*Argyropoulos! When a client wants to enter in an Auction */
+	public static void enterAuction(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter the auction ID");
+		int auctionID = sc.nextInt();
+		/*Argyropoulos! checks to our AuctionNumbers Arraylist if the auctionID of the user it exists*/
+		if (AuctionNumbers.contains(auctionID)) {
+			/*Argyropoulos! probably make the auction room */
+			System.out.println("YEA");
+		}
+		
+	}
+
+    public static void main(String [] argv) throws Exception {
+		int port, maxLength = 255;
+		String hostname;
+		String lineToServer, lineFromServer;
+		
+		/* First argument is the running server's name */
+		hostname = new String( argv[0] );
+
+		/* Second argument is the port in which the server accepts connections */
+		port = Integer.parseInt( argv[1] );
+
+		/* Determine the IP address of the server from the hostname */
+		InetAddress serverAddr = InetAddress.getByName(hostname);
+
+		while ( true ) {
+			/*Argyropoulos! Give to the client the options */
+			System.out.println( "Type auction :enter to an auction with auction ID\nType item:List an Item\nType exit: quit");
+
+			/* Create a buffer to hold the user's input */
+			BufferedReader userInput = new BufferedReader( new InputStreamReader ( System.in ) );
+
+			/* Get the user's input */
+			lineToServer = userInput.readLine();
+
+			/*Argyropoulos! when the client want to list an item for auction */
+			if ( lineToServer.equals( "item" ) )
+				listItem();
+			/*Argyropoulos! when the client want to enter to an auction */
+			if ( lineToServer.equals( "auction" ) )
+				enterAuction();
+
+
+			/* Stop infinite loop if user wants to stop getting echos by typing exit */
+			if ( lineToServer.equals( "exit" ) )
+				break;
+
+			/* Create array of 255 bytes to hold outgoing message */
+			byte[] data = new byte[maxLength];
+
+			/* Convert the string message into bytes */
+			data = lineToServer.getBytes();
+
+			/* Create datagram to send to server specifying message, message length, server address, port */
+			DatagramPacket outToServer = new DatagramPacket( data, data.length, serverAddr, port );
+
+			/* Create a datagram socket through which the data will be send */
+			DatagramSocket socket = new DatagramSocket();
+
+			/* Send the datagram through the socket */
+			socket.send( outToServer );
+
+			/* Create array of 255 raw bytes to hold incoming message */
+			byte [] response = new byte[maxLength];
+		
+			/* Create a datagram to receive from server specifying the message received */
+			DatagramPacket inFromServer = new DatagramPacket( response, maxLength );
+
+			/* Receive the echo datagram from server (capitalized) */
+			socket.receive( inFromServer );
+
+			/* Convert received byte array to string for displaying */
+			lineFromServer = new String( inFromServer.getData(), 0, inFromServer.getLength());
+
+			/* Output echoed message to the screen */
+			System.out.println( "Received: " + lineFromServer );
+		}
+	}
+}
+
+/*
+ * Example:
+ *   java UDPEchoClient 192.168.12.12 4567
+ * Output:
+ *	 Type text to send to server:
+ *   Hello server.
+ *   Received: HELLO SERVER.
+ */
+		
+/*** EXTRA INFORMATION ***/		
+/*
+DatagramPacket: it implements UDP Socket communication.
+DatagramSocket: it is used for UDP communication.
+BufferedReader: it supports input buffering. It provides the readLine() method for reading an entire line at a
+                time from a stream.
+InputStreamReader: reads a stream. It is used to convert between byte streams and character streams. It provides
+                   a bridge between byte-oriented and character-oriented input streams.
+*/
+
+/*
+If the server is not 'alive' the program blocks. To resolve this, you need to catch the exceptions and handle them.
+*/
---
+++ Server.java
@@ -0,0 +1,71 @@
+/*
+ * Filename: UDPEchoServer.java
+ * Description: An echo server using connectionless delivery system (UDP).
+ *              Receives character messages at a specified (hardcoded) port.
+ *              The message is send back to the client capitalized.
+ *              No error handling and exceptions are implemented.
+ * Operation: java UDPEchoServer
+ *
+ * Author: Thanos Hatziapostolou
+ * Module: Network Computing
+ */
+
+import java.net.*;
+import java.io.*;
+
+public class Server {
+   public static void main( String argv[] ) throws Exception {
+      String capitalizedSentence = null; 
+      int port = 4567;
+	  int maxLength = 255;
+
+	  /* Create a receiving datagram data buffer */
+	  byte[] buffer = new byte[255];
+
+	  /* Create receiving datagram object of maximum size maxLength */
+	  DatagramPacket indatagram = new DatagramPacket( buffer, maxLength );
+
+	  /* Create a UDP socket on a specific port */
+	  DatagramSocket socket = new DatagramSocket( port );
+
+	  /* Display message that the UDP echo server is running */
+      System.out.println( "Starting a UDP Echo Server on port " + port );
+
+	  while( true ) {
+		/* Set the max length of datagram to 255 */
+		indatagram.setLength( maxLength );
+	
+		/* Receive the datagram from the client  */
+		socket.receive( indatagram );
+		
+		/* Convert the message from the byte array to a string array for displaying */
+		String msgFromClient = new String( indatagram.getData(), 0, indatagram.getLength() );
+		
+		/* Display the message on the screen */
+		System.out.println( "\nMessage received from " + indatagram.getAddress() + " from port "
+							+ indatagram.getPort() + ".\nContent: " + msgFromClient );
+		
+		/* Capitalize the received message */
+		capitalizedSentence = msgFromClient.toUpperCase() + '\n';
+		
+		/* Create a new datagram data buffer (byte array) for echoing capitalized message */
+		byte[] msgToClient = capitalizedSentence.getBytes(); 
+		
+		/* Create an outgoing datagram by extracting the client's address and port from incoming datagram */
+		DatagramPacket outdatagram = new DatagramPacket( msgToClient, msgToClient.length,
+									 indatagram.getAddress(), indatagram.getPort() );
+		
+		/* Send the reply back to the client */
+		socket.send( outdatagram );
+	  }
+   }
+}
+
+/*
+ * Example:
+ *   java UDPEchoServer
+ * Output:
+ *	 Starting a UDP Echo Server on port 4567:
+ *   Message Received from: hatziapostolou/197.87.76.3 on port 1354
+ *   Content: Hello server
+ */
